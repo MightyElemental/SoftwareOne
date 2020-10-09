5d8cc74c3e70bc5f865e778f9b5586c6a33bf935
MightyElemental <MightyElemental@gmail.com>
2020-10-09 22:26:59 +0100

    Improved palindrome finder with regex

--- Week Three/main_w3.py
+++ Week Three/main_w3.py
@@ -1,4 +1,4 @@
-import math
+import math, re
 # E1
 # Write a program to keep asking for a number until you enter a negative number. At the end, print the sum of all entered numbers.
 # Write a program to keep asking for a number until you enter a negative number. At the end, print the average of all entered numbers.
@@ -18,7 +18,10 @@ print("So the average is",round(sum/entry_count,1),"to 1 d.p.")
 # ---- PRACTICAL ----
 
 # Write a program that take a sentence or a word as an input and print if it is a palindrome or not.
+# ‘Dammit, I'm mad!’ is also considered a palindrome when neither punctuation nor spaces are taken into account.
+# Change your program so it can recognise these palindromes too.
 phrase = input("Enter a phrase> ").lower()
+phrase = re.sub("[^a-z]","", phrase) # use regex to remove any non-letter
 flag = False
 for i in range(math.ceil(len(phrase)/2)):
     s_letter = phrase[i]
@@ -30,5 +33,3 @@ if(flag):
     print("This is NOT palindromic")
 else:
     print("This IS palindromic")
-
-
