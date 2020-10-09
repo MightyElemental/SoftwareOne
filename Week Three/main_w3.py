610cd26dbc29255b2639acd918b5c1187d94a18b
MightyElemental <MightyElemental@gmail.com>
2020-10-09 22:19:20 +0100

    Added palindrome finder

--- Week Three/main_w3.py
+++ Week Three/main_w3.py
@@ -1,4 +1,4 @@
-
+import math
 # E1
 # Write a program to keep asking for a number until you enter a negative number. At the end, print the sum of all entered numbers.
 # Write a program to keep asking for a number until you enter a negative number. At the end, print the average of all entered numbers.
@@ -14,3 +14,21 @@ while(num >= 0):
 print("You entered",entry_count,"numbers")
 print("The total of the numbers you entered is:",sum)
 print("So the average is",round(sum/entry_count,1),"to 1 d.p.")
+
+# ---- PRACTICAL ----
+
+# Write a program that take a sentence or a word as an input and print if it is a palindrome or not.
+phrase = input("Enter a phrase> ").lower()
+flag = False
+for i in range(math.ceil(len(phrase)/2)):
+    s_letter = phrase[i]
+    e_letter = phrase[len(phrase)-1-i]
+    if(s_letter != e_letter):
+        flag = True
+        break
+if(flag):
+    print("This is NOT palindromic")
+else:
+    print("This IS palindromic")
+
+
