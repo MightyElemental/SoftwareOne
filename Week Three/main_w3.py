13058f089f192043f5ed94718e0084056fadb9b7
MightyElemental <MightyElemental@gmail.com>
2020-10-09 23:36:05 +0100

    Completed Week 3 Practical 2

--- Week Three/main_w3.py
+++ Week Three/main_w3.py
@@ -40,13 +40,45 @@ else:
 # Write a script that takes a sentence from the user without any punctuation and prints
 # the sentence without any white spaces. Note a white space is represented by ' ', and
 # an empty string is represented by ''.
-print(re.sub(r"\s","",input("Enter a sentence> ")))
+print(re.sub(r"\s", "", input("Enter a sentence> ")))
 
 # Same as above except that each word in the output should start with a upper case letter
 # and all other letter should be lower case (also known as CamelCase).
-print(re.sub(r"(\b[a-z])", lambda x : x.group(1).upper() ,input("Enter a sentence> ").lower()))
+print(re.sub(r"(\b[a-z])", lambda x : x.group(1).upper(), input("Enter a sentence> ").lower()))
 
 # Write a script that takes a sentence from a user written in CamelCase (without any blank
 # spaces), creates the list of words from that sentence, and then prints that list.
 # Example input: ThisIsACamelCaseSentenceWithNoSpaces
-print(re.findall(r"([A-Z].*?)(?=[A-Z]|$)",input("Enter a sentence> ")))
+print(re.findall(r"([A-Z].*?)(?=[A-Z]|$)", input("Enter a sentence> ")))
+
+# ---- Exercise 3 ----
+
+# Write a script that encrypts a plain text into a cypher text using the Caesar Cipher algorithm.
+phrase = input("Enter a message> ").lower()
+shift_raw = input("a=").lower()
+shift = ord(shift_raw)-97
+encrypted_phrase = ""
+for l in phrase:
+    if(l == " "): # don't encrypt spaces
+        encrypted_phrase += " "
+        continue
+    # shift to a=0, then add shift, then ensure it's within 26 letter limit, then shift to a=97
+    letter_value = ( ( ord(l) - 97 ) + shift ) % 26 + 97
+    encrypted_phrase += chr(letter_value)
+print("Encrypted message:")
+print(encrypted_phrase)
+
+# Write a script that decrypts a cipher text into a plain text using the Caesar Cipher algorithm.
+phrase = input("Enter an encrypted message> ").lower()
+shift_raw = input("a=").lower()
+shift = ord(shift_raw)-97
+decrypted_phrase = ""
+for l in phrase:
+    if(l == " "): # don't decrypt spaces
+        decrypted_phrase += " "
+        continue
+    # shift to a=0, then sub shift, then ensure it's within 26 letter limit, then shift to a=97
+    letter_value = ( ( ord(l) - 97 ) - shift ) % 26 + 97
+    decrypted_phrase += chr(letter_value)
+print("Decrypted message:")
+print(decrypted_phrase)
