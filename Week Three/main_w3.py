f53a232d5a72653ea048f4eb9ca679bb99695cf7
MightyElemental <MightyElemental@gmail.com>
2020-10-11 23:59:51 +0100

    Tidied code

--- Week Three/main_w3.py
+++ Week Three/main_w3.py
@@ -21,22 +21,24 @@ print("So the average is",round(sum/entry_count,1),"to 1 d.p.")
 # Write a program that take a sentence or a word as an input and print if it is a palindrome or not.
 # ‘Dammit, I'm mad!’ is also considered a palindrome when neither punctuation nor spaces are taken into account.
 # Change your program so it can recognise these palindromes too.
+def isPalindomic(phrase: str) -> bool:
+    for i in range(math.ceil(len(phrase)/2)):
+        s_letter = phrase[i]
+        e_letter = phrase[len(phrase)-1-i]
+        if(s_letter != e_letter):
+            return False
+    return True
+
 phrase = input("Enter a phrase> ").lower()
-phrase = re.sub("[^a-z]","", phrase) # use regex to remove any non-letter
-flag = False
-for i in range(math.ceil(len(phrase)/2)):
-    s_letter = phrase[i]
-    e_letter = phrase[len(phrase)-1-i]
-    if(s_letter != e_letter):
-        flag = True
-        break
-if(flag):
-    print("This is NOT palindromic")
-else:
+phrase = re.sub("[^a-z]", "", phrase) # use regex to remove any non-letter
+
+if(isPalindomic(phrase)):
     print("This IS palindromic")
+else:
+    print("This is NOT palindromic")
 
 # Written bt Charlie (xo#1111)
-def palindrome(word):
+def palindrome(word: str) -> bool:
     return word == word[::-1]
 
 # ---- Exercise 2 ----
@@ -53,36 +55,44 @@ print(re.sub(r"(\b[a-z])", lambda x : x.group(1).upper(), input("Enter a sentenc
 # Write a script that takes a sentence from a user written in CamelCase (without any blank
 # spaces), creates the list of words from that sentence, and then prints that list.
 # Example input: ThisIsACamelCaseSentenceWithNoSpaces
-print(re.findall(r"([A-Z].*?)(?=[A-Z]|$)", input("Enter a sentence> ")))
+print(re.findall(r"([A-Z].*?)(?=[A-Z]|$)", input("Enter a sentence written in CamelCase with no spaces> ")))
 
 # ---- Exercise 3 ----
 
 # Write a script that encrypts a plain text into a cypher text using the Caesar Cipher algorithm.
+def caeserEncrypt(msg: str, shift: int) -> str:
+    encrypted_phrase = ""
+    for l in msg:
+        # don't encrypt spaces
+        if(l == " "):
+            encrypted_phrase += " "
+            continue
+        # shift to a=0, then add shift, then ensure it's within 26 letter limit, then shift to a=97
+        letter_value = ( ( ord(l) - 97 ) + shift ) % 26 + 97
+        encrypted_phrase += chr(letter_value)
+    return encrypted_phrase
+
 phrase = input("Enter a message> ").lower()
 shift_raw = input("a=").lower()
 shift = ord(shift_raw)-97
-encrypted_phrase = ""
-for l in phrase:
-    if(l == " "): # don't encrypt spaces
-        encrypted_phrase += " "
-        continue
-    # shift to a=0, then add shift, then ensure it's within 26 letter limit, then shift to a=97
-    letter_value = ( ( ord(l) - 97 ) + shift ) % 26 + 97
-    encrypted_phrase += chr(letter_value)
 print("Encrypted message:")
-print(encrypted_phrase)
+print(caeserEncrypt(phrase, shift))
 
 # Write a script that decrypts a cipher text into a plain text using the Caesar Cipher algorithm.
+def caeserDecrypt(msg: str, shift: int) -> str:
+    decrypted_phrase = ""
+    for l in msg:
+        # don't decrypt spaces
+        if(l == " "):
+            decrypted_phrase += " "
+            continue
+        # shift to a=0, then sub shift, then ensure it's within 26 letter limit, then shift to a=97
+        letter_value = ( ( ord(l) - 97 ) - shift ) % 26 + 97
+        decrypted_phrase += chr(letter_value)
+    return decrypted_phrase
+
 phrase = input("Enter an encrypted message> ").lower()
 shift_raw = input("a=").lower()
 shift = ord(shift_raw)-97
-decrypted_phrase = ""
-for l in phrase:
-    if(l == " "): # don't decrypt spaces
-        decrypted_phrase += " "
-        continue
-    # shift to a=0, then sub shift, then ensure it's within 26 letter limit, then shift to a=97
-    letter_value = ( ( ord(l) - 97 ) - shift ) % 26 + 97
-    decrypted_phrase += chr(letter_value)
 print("Decrypted message:")
-print(decrypted_phrase)
+print(caeserDecrypt(phrase, shift))
--- Week Two/main.py
+++ Week Two/main.py
@@ -2,7 +2,11 @@ import math
 
 LBS_PER_KG = 2.20462
 
-#Q1
+# ---- Exercise 1 ----
+
+# Write a series of small script that convert weight, distance, and liquid measurement
+# from Imperial to Metric system.
+# Write the reverse conversion.
 
 stones = int(input("stones> "))
 pounds = int(input("pounds> "))
@@ -15,7 +19,11 @@ kg = int(input("KG> "))
 total_pounds = LBS_PER_KG*kg
 print(kg,"kilograms is",round(total_pounds/14,0),"stone",round(total_pounds%14,1),"pounds")
 
-#Q3
+# ---- Exercise 3 ----
+
+# A fruit company sells bananas for £3.00 a kilogram plus £4.99 per order for postage and
+# packaging. If an order is over £50.00, the P&P is reduced by £1.50. Write a script that will take
+# the number of kilo of bananas as a user input and print the cost of that order.
 def getPrice(kg):
     cost = 3*kg
     postage = 4.99
@@ -26,7 +34,17 @@ def getPrice(kg):
 kg = int(input("How many kilos of bananas? "))
 print(getPrice(kg))
 
-#Q4
+# ---- Exercise 4 ----
+
+# Write a script that take the age and rate (the heart rate) that print a description of a person's
+# training zone based on his or her age and training heart rate, rate. The zone is determined
+# by comparing rate with the person's maximum heart rate m:
+# Interval range        |   Training Zone
+# rate ≥ 0.9 m          |   Interval training
+# 0.7 m ≤ rate < 0.9 m  |   Threshold training
+# 0.5 m ≤ rate < 0.7 m  |   Aerobic training
+# rate < 0.5 m          |   Couch potato
+# The maximum heart rate in beats per minute is given by the formula: m = 208 − 0.7 * age
 def getState(age,rate):
     m = 208 - 0.7*age
     if(rate > 0.9*m):
@@ -39,8 +57,12 @@ def getState(age,rate):
 
 print(getState(19,120))
 
-#Q5
-def heron(a,b,c):
+# ---- Exercise 5 ----
+
+# Write a script that takes the lengths of the sides of a triangle (a, b, and c) from the user and then
+# print the area of the triangle using Heron's formula. (Look up Heron's formula if you do not
+# remember it.). Note, to compute x^n using Python, you must use the function pow(x,n).
+def heron(a: float, b: float, c: float) -> float:
     s = (a+b+c)/2
     return math.sqrt(s*(s-a)*(s-b)*(s-c))
 
