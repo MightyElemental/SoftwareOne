c1424b3e5d4f42aff49573ac459c75b93db1365d
MightyElemental <MightyElemental@gmail.com>
2020-10-12 14:56:43 +0100

    Added question text

--- Challenges/prime_numbers.py
+++ Challenges/prime_numbers.py
@@ -1,6 +1,40 @@
 import math, os
 
-# Challenge 1
+'''
+Challenge I: prime numbers
+THE PROBLEM
+A prime number (or a prime) is a natural number greater than 1 that has
+no positive divisors other than 1 and itself.
+The property of being prime is called primality. A simple but slow method of
+verifying the primality of a given number is known as trial division. It
+consists of testing whether is a multiple of any integer between 2 and√ .
+THE BASIC
+Write a program that, given a number comprised between 2 and 49, returns
+if it is a prime number or not. We can assume that the computer knows
+(stores) that [2, 3, 5, 7] are prime numbers.
+THE ADVANCED BIT
+Write a program that, given a number greater than 2, returns if it is a
+prime number or not. We can assume that the computer at the start knows
+only that 2 is prime number. We should use a loop to test several numbers.
+THE CLEVER ONE
+Write a program that, given a number greater than 2, returns if it is a
+prime number or not. We can assume that the computer at the start knows
+only that 2 is prime number. Every time the program is ran, it should
+remember the prime numbers it has found before.
+THE OLYMPIAN ONE
+Taken from the 2012 British Informatics Olympiad.
+Every integer greater than 1 can be uniquely expressed as the product of
+prime numbers (ignoring reordering those numbers). This is called the
+prime factorisation of the number.
+For example:
+• 100 = 2 2 5 5
+• 101 = 101 (since 101 is a prime number)
+We are interested in the product of the distinct prime factors of a given
+number; in other words each number in the prime factorisation is to be used
+only once. Since 100 = 2 2 5 5 the product we require is 10 (i.e. 2
+5). Write a program which reads in a single integer n (1 < n < 1,000,000)
+and outputs a single integer, the product of the distinct prime factors of n.
+'''
 
 # Ensure cache file is located next to the script for ease
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -17,7 +51,7 @@ else:
     prime_list = [int(x) for x in f.readlines()]
     f.close()
 
-def isPrime(number: int):
+def isPrime(number: int) -> bool:
     if(number <= 3):
         return number > 1
     for i in range(2, math.floor(number**0.5)):
@@ -45,7 +79,7 @@ def generateAndCachePrimes(upper_limit: int):
     f.close()
     #print("INFO:Cached prime numbers")
 
-def primeFactorize(number: int):
+def primeFactorize(number: int) -> list:
     factor_list = []
     if(isPrime(number)):
         return [number]
