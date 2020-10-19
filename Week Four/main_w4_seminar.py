14c5cc5bca35824fb8b8d5798c24a0410f6327ee
MightyElemental <MightyElemental@gmail.com>
2020-10-19 20:01:23 +0100

    Completed Exercise 2 (in pain)

--- Week Four/main_w4_seminar.py
+++ Week Four/main_w4_seminar.py
@@ -41,3 +41,88 @@ print(find_pairs([-1, 1, 2, 4, 8], 7)) # [(-1, 8)]
 print(find_pairs([2, 4, 5, 7], 9)) # [(2, 7), (5, 4)]
 print(find_pairs([2, 4, 5, 7], 8)) # []
 print(find_pairs([-3, -1, 2, 3, 5, 9, 11], 8)) # [(-3, 11), (3, 5), (9, -1)]
+
+
+# ---- Exercise 2 ----
+'''
+Write in pseudo code a function merge(listA: List, listB: List) that returns a
+sorted list containing the elements of both list where listA and listB are two sorted lists
+of integers. If an element exists in both lists, it must appear multiple times in the returned list.
+For example:
+>>> merge([1,3,4,7],[2,3,5])
+[1,2,3,3,4,5,7]
+'''
+
+# Just want to comment that I hate pseudocode with a passion, and see no use for it.
+# Especially when the syntax is more complex than any (not every) programming language.
+
+'''
+Function merge(listA: List, listB: List): List
+    result := Array[0..(listA.length+listB.length)] of int
+    aindex = 0 : int
+    bindex = 0 : int
+    while aindex < listA.length || bindex < listB.length do
+        if bindex >= listB.length then
+            result[aindex+bindex] = listA[aindex]
+            aindex = aindex + 1
+            continue
+        endif
+        if aindex >= listA.length then
+            result[aindex+bindex] = listB[bindex]
+            bindex = bindex + 1
+            continue
+        endif
+        if listA[aindex] <= listB[bindex] then
+            result[aindex+bindex] = listA[aindex]
+            aindex = aindex + 1
+        else
+            result[aindex+bindex] = listB[bindex]
+            bindex = bindex + 1
+        endif
+    endwhile
+    return result
+'''
+
+def merge(listA: list, listB: list) -> list:
+    result = []
+    aindex = 0
+    bindex = 0
+    while(aindex < len(listA) or bindex < len(listB)):
+        if bindex >= len(listB):
+            result.append(listA[aindex])
+            aindex = aindex + 1
+            continue
+        if aindex >= len(listA):
+            result.append(listB[bindex])
+            bindex = bindex + 1
+            continue
+        if listA[aindex] <= listB[bindex]:
+            result.append(listA[aindex])
+            aindex = aindex + 1
+        else:
+            result.append(listB[bindex])
+            bindex = bindex + 1
+    return result
+
+print(merge([1,3,4,7],[2,3,5]))
+
+# I am in pain due to the pseudocode
+
+
+# ---- Exercise 3: reinventing the wheel! ----
+'''
+For this question we are emulating the method split() from the type str. Write the
+algorithm for a function splitText(text:String, delimiters:String) which
+returns the list of the words by splitting the string text at each delimiters. The delimiters
+themselves are discarded. An example is given below:
+
+>>> sampleText = "As Python's creator, I'd like to say a
+few words about its origins."
+>>> splitText(sampleText, ", '.")
+['As', 'Python', 's', 'creator', 'I', 'd', 'like', 'to',
+'say', 'a', 'few', 'words', 'about', 'its', 'origins']
+
+You can assume that a string has a method contains(Character) that returns true if the
+character is in the string, false otherwise. This exercise is more challenging than it may look
+like.
+'''
