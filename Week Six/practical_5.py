a4cd99e5ebd7dca10edc8225bd65bb1412a1b322
MightyElemental <MightyElemental@gmail.com>
2020-11-04 20:48:42 +0000

    Completed Exercise 1&2

--- Week Six/practical_5.py
+++ Week Six/practical_5.py
@@ -1,3 +1,4 @@
+import re
 sample_text = (" As Python s creator, I'd like to say a few words about its "+
               "origins adding a bit of personal philosophy. "+
               "Over six years ago in December I was looking for a "+
@@ -12,3 +13,28 @@ sample_text = (" As Python s creator, I'd like to say a few words about its "+
               "mood and a big fan of Monty Python s Flying Circus.  ")
 
 ######################### EXERCISE 1 ##########################################
+
+def split_text(text: str, delimiters:str="") -> list:
+    result = []
+    buffer = ""
+    for c in text:
+        if c in delimiters:
+            if len(buffer) > 0:
+                result.append(buffer)
+                buffer=""
+        else:
+            buffer += c
+    if(len(buffer) > 0):
+        result.append(buffer)
+    return result
+
+def get_words_frequencies(text: str):
+    # Gets all words and ignores cases and ignores punctuation
+    words = re.findall(r'(\b[a-z]+)',text.lower())
+    result = {}
+    for word in words:
+        if word not in result:
+            result[word] = 1
+        else:
+            result[word] += 1
+    return result
